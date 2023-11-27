c4f9093a0ad824a9847f2a7805f6a150f3788dc3
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-28 00:38:44 +0600

    validate login form using react-hook-form

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,70 +1,84 @@
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
-    console.log({
-      email: data.get('email'),
-      password: data.get('password'),
-    });
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
   };
 
   return (
-      <Container component="main" maxWidth="xs" sx={{mb: "50px"}}>
+    <Container component="main" maxWidth="xs" sx={{ mb: "50px" }}>
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Typography component="h2" fontWeight={600} variant="h5">
+          Login
+        </Typography>
         <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
+          component="form"
+          onSubmit={handleSubmit(onSubmit)}
+          noValidate
+          sx={{ mt: 1 }}
         >
-          <Typography component="h2" fontWeight={600} variant="h5">
+          <TextField
+            margin="normal"
+            fullWidth
+            label="Email Address"
+            {...register("email", { required: true })}
+            type="email"
+          />
+          {errors.email && (
+            <Typography color="red">Email is required!</Typography>
+          )}
+          <TextField
+            margin="normal"
+            fullWidth
+            {...register("password", {
+              required: true,
+              minLength: 6,
+            })}
+            label="Password"
+            type="password"
+          />
+          {errors.password?.type === "required" && (
+            <Typography color="red">Pasword is required!</Typography>
+          )}
+          {errors.password?.type === "minLength" && (
+            <Typography color="red">
+              Pasword must be 6 characters or longer!
+            </Typography>
+          )}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
             Login
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Login
-            </Button>
-            <Grid textAlign="center">
-                <Link href="/signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-            </Grid>
-          </Box>
+          </Button>
+          <Grid textAlign="center">
+            <Link href="/signup" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Grid>
         </Box>
-      </Container>
+      </Box>
+    </Container>
   );
 }
