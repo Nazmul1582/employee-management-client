092b793b75fc9f83b026aafcf6aa3ff6c0032d8a
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-29 01:24:55 +0600

    changed employee table design

--- src/pages/Dashboard/EmployeeList/EmployeeList.jsx
+++ src/pages/Dashboard/EmployeeList/EmployeeList.jsx
@@ -1,35 +1,77 @@
+import { Button, IconButton } from "@mui/material";
+import CloseIcon from '@mui/icons-material/Close';
+// import CheckIcon from '@mui/icons-material/Check';
 import useUser from "../../../hooks/useUser";
-import { DataGrid } from '@mui/x-data-grid';
+import { DataGrid } from "@mui/x-data-grid";
 
 const columns = [
-  { field: 'name', headerName: 'Name', width: 70 },
-  { field: 'email', headerName: 'Email', width: 130 },
-  { field: 'varified', headerName: 'Varified', width: 130 },
-  { field: 'bank_account_no', headerName: 'Bank Account', width: 130 },
-  { field: 'salary', headerName: 'Salary', width: 130 },
-  { field: 'pay', headerName: 'Pay', width: 130 },
-  { field: 'details', headerName: 'Details', width: 130 },
+  { field: "name", headerName: "Name", width: 130 },
+  { field: "email", headerName: "Email", width: 180 },
+  {
+    field: "varified",
+    headerName: "Varified",
+    width: 80,
+    renderCell: (params) => (
+      <IconButton onClick={() => handleVarify(params.row.id)} aria-label="delete">
+        <CloseIcon />
+      </IconButton>
+    ),
+  },
+  { field: "bank_account_no", headerName: "Bank Account", width: 130 },
+  { field: "salary", headerName: "Salary", width: 80 },
+  {
+    field: "pay",
+    headerName: "Pay",
+    width: 80,
+    renderCell: (params) => (
+      <Button
+        variant="contained"
+        onClick={() => handlePay(params.row.id)}
+      >
+        Pay
+      </Button>
+    ),
+  },
+  {
+    field: "details",
+    headerName: "Details",
+    width: 100,
+    renderCell: (params) => (
+      <Button
+        onClick={() => handleButtonClick(params.row.id)}
+        variant="outlined"
+      >
+        Details
+      </Button>
+    ),
+  },
 ];
+const handleVarify = (id) => {
+  console.log(id);
+};
+const handlePay = (id) => {
+  console.log(id);
+};
+const handleButtonClick = (id) => {
+  console.log(id);
+};
 
 const EmployeeList = () => {
-    const [users] = useUser();
-    console.log(users);
-    const userRow = users.map(ele => {
-        return{
-            id: ele._id,
-            name: ele.name,
-            email: ele.email,
-            varified: false,
-            bank_account_no: ele.bank_account_no,
-            salary: ele.salary,
-            pay: "pay btn",
-            details: "details"
-        }
-    })
-    return (
-        <div style={{ height: 400, width: '100%' }}>
+  const [users] = useUser();
+  const rows = users.map((ele) => {
+    return {
+      id: ele._id,
+      name: ele.name,
+      email: ele.email,
+      varified: false,
+      bank_account_no: ele.bank_account_no,
+      salary: ele.salary,
+    };
+  });
+  return (
+    <div style={{ height: 400, width: "100%" }}>
       <DataGrid
-        rows={userRow}
+        rows={rows}
         columns={columns}
         initialState={{
           pagination: {
@@ -40,7 +82,7 @@ const EmployeeList = () => {
         checkboxSelection
       />
     </div>
-    );
+  );
 };
 
 export default EmployeeList;
