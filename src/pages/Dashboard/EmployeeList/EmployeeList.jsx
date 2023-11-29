05891b4fb7250c5f4309b50b888c258381ebb708
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-30 00:08:13 +0600

    created implement employee pay salary functionality with show success or failure alert

--- src/pages/Dashboard/EmployeeList/EmployeeList.jsx
+++ src/pages/Dashboard/EmployeeList/EmployeeList.jsx
@@ -1,13 +1,16 @@
-import { Button, IconButton } from "@mui/material";
+import {
+  Button, IconButton
+} from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
 import CheckIcon from "@mui/icons-material/Check";
 import useUser from "../../../hooks/useUser";
 import { DataGrid } from "@mui/x-data-grid";
-import axiosPublic from "../../../utils/AxiosPublic";
 import Swal from "sweetalert2";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 
 const EmployeeList = () => {
   const [users] = useUser();
+  const axiosSecure = useAxiosSecure()
   const columns = [
     { field: "name", headerName: "Name", width: 130 },
     { field: "email", headerName: "Email", width: 180 },
@@ -31,9 +34,13 @@ const EmployeeList = () => {
       headerName: "Pay",
       width: 80,
       renderCell: (params) => (
-        <Button disabled={params.row.isVerified ? false : true} variant="contained" onClick={() => handlePay(params.row.id)}>
-          Pay
-        </Button>
+          <Button
+            disabled={params.row.isVerified ? false : true}
+            variant="contained"
+            onClick={() => handlePay(params.row.id)}
+          >
+            Pay
+          </Button>
       ),
     },
     {
@@ -50,7 +57,7 @@ const EmployeeList = () => {
       ),
     },
   ];
-  
+
   const handleVerify = (id) => {
     Swal.fire({
       title: "Are you sure?",
@@ -59,48 +66,88 @@ const EmployeeList = () => {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, verify user!"
-    }).then(async(result) => {
+      confirmButtonText: "Yes, verify user!",
+    }).then(async (result) => {
       if (result.isConfirmed) {
-        try {          
+        try {
           const isVerifiedUser = { isVerified: true };
-          const res = await axiosPublic.patch(`/users/${id}`, isVerifiedUser);
-          if(res.data.modifiedCount > 0){
+          const res = await axiosSecure.patch(`/users/${id}`, isVerifiedUser);
+          if (res.data.modifiedCount > 0) {
             Swal.fire({
               title: "Verified!",
               text: "Your user has been verified.",
-              icon: "success"
+              icon: "success",
             });
           }
         } catch (error) {
           Swal.fire({
             title: "Oops!",
             text: error.message,
-            icon: "error"
+            icon: "error",
           });
         }
       }
     });
   };
 
-  const handlePay = (id) => {
-    console.log(id);
+  const handlePay = async(id) => {
+    const res = await axiosSecure.get(`/users/${id}`);
+
+    const { value: formValues } = await Swal.fire({
+      title: 'Enter Salary Information',
+      html:`
+        <label for="salary">Salary</label>
+        <input type="number" id="salary" class="swal2-input" value=${res.data.salary}>
+        <label for="month">Month</label>
+        <input type="text" id="month" class="swal2-input" placeholder="Enter month">
+        <label for="year">Year</label>
+        <input type="number" id="year" class="swal2-input" placeholder="Enter year">        
+      `,
+      preConfirm: () =>  {
+        const salary = document.getElementById('salary').value;
+        const month = document.getElementById('month').value;
+        const year = document.getElementById('year').value;
+        const transactionId = `tid-${Date.now()}`
+        const employee = res.data.email;
+        const payDetails = {employee, salary, month, year, transactionId};
+        return payDetails;
+      }
+    });
+    if (formValues) {
+      formValues.salary = parseInt(formValues.salary)
+      formValues.year = parseInt(formValues.year)
+      try {
+        const res = await axiosSecure.post(`/users/employee/pay-salary`, formValues)
+        if(res.data._id){
+          Swal.fire({
+            title: "You have paid the salary!",
+            icon: "success",
+            timer: 1500,
+            showConfirmButton: false
+          });
+        }        
+      } catch (error) {
+        Swal.fire("Oops!", error.message, "error");
+      }
+    }
   };
-  
+
   const handleButtonClick = (id) => {
     console.log(id);
   };
 
-  const rows = users.filter(user => user.userRole !== "admin" && user.userRole !== "hr").map((ele) => {
-    return {
-      id: ele._id,
-      name: ele.name,
-      email: ele.email,
-      isVerified: ele.isVerified,
-      bank_account_no: ele.bank_account_no,
-      salary: ele.salary,
-    };
-  });
+  const rows = users
+    .filter((user) => user.userRole !== "admin" && user.userRole !== "hr")
+    .map((ele) => {
+      return {
+        id: ele._id,
+        name: ele.name,
+        email: ele.email,
+        isVerified: ele.isVerified,
+        bank_account_no: ele.bank_account_no,
+        salary: ele.salary,
+      };
+    });
   return (
     <div style={{ height: 400, width: "100%" }}>
       <DataGrid
