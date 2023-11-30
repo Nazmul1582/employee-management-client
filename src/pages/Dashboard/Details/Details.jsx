4adbc8bcb5f78a6511b920451e0b69fdd9ae39db
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-30 06:45:06 +0600

    displayed employee name, image and designation in details page

--- src/pages/Dashboard/Details/Details.jsx
+++ src/pages/Dashboard/Details/Details.jsx
@@ -1,17 +1,30 @@
-import { Box, Container, Typography } from '@mui/material';
-import { useLoaderData } from 'react-router-dom';
+import { Avatar, Box, Container, Paper, Typography } from "@mui/material";
+import { useLoaderData } from "react-router-dom";
 
 const Details = () => {
-    const {data: user} = useLoaderData();
-    return (
-        <Box>
-            <Container maxWidth="xl">
-            <Typography>The details information of {user.name}</Typography>            
-            <Typography>PhotoURL: {user.image}</Typography>
-            <Typography>Designation: {user.designation}</Typography>
-            </Container>
-        </Box>
-    );
+  const { data: user } = useLoaderData();
+  return (
+    <Box>
+      <Container maxWidth="xl">
+        <Paper
+          elevation={6}
+          sx={{ textAlign: "center", p: 5 }}
+        >
+          <Avatar
+            src={user.image}
+            alt="employee photo"
+            sx={{ width: 150, height: 150, mx: "auto", mb: 3 }}
+          />
+          <Typography component="h2" variant="h6" fontWeight={600}>
+            Employee Name: {user.name}
+          </Typography>
+          <Typography variant="h6" fontWeight={500}>
+            Designation: {user.designation}
+          </Typography>
+        </Paper>
+      </Container>
+    </Box>
+  );
 };
 
 export default Details;
