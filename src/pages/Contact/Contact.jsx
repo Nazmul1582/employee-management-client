a33c0d48a209f61c7081596037e6f13a96195bd8
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-11-30 19:55:46 +0600

    added a address in contact page

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,12 +1,60 @@
-import { Button, Container, Grid, Paper, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
 
 const Contact = () => {
+  const address = {
+    street: "123 Main Street",
+    city: "Anytown",
+    state: "CA",
+    zipCode: "12345",
+    country: "United States",
+  };
+
   return (
     <Container maxWidth="xl" sx={{ py: "80px" }}>
-      <Grid container spacing={3}>
-        <Grid item xs={12}></Grid>
-        <Grid item xs={12}>
-          <Paper component="form" elevation={2} sx={{padding: 5, display: "flex", flexDirection: "column", gap: 3, width: "80%", mx: "auto"}}>
+      <Grid container spacing={5}>
+        <Grid item xs={12} md={6} lg={4}>
+          <Paper
+            elevation={3}
+            style={{
+              padding: 5,
+              textAlign: "center",
+              display: "grid",
+              justifyContent: "center",
+              alignItems: "center",
+              height: "100%",
+            }}
+          >
+            <Box>
+              <Typography variant="h6" fontWeight={600}>Address</Typography>
+              <Typography>Street: {address.street}</Typography>
+              <Typography>City: {address.city}</Typography>
+              <Typography>State: {address.state}</Typography>
+              <Typography>Zip Code: {address.zipCode}</Typography>
+              <Typography>Country: {address.country}</Typography>
+            </Box>
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={6} lg={8}>
+          <Paper
+            component="form"
+            elevation={2}
+            sx={{
+              padding: 5,
+              display: "flex",
+              flexDirection: "column",
+              gap: 3,
+              mx: "auto",
+            }}
+          >
+            <Typography variant="h6" fontWeight={600} textAlign="center">Contact Us</Typography>
             <TextField id="outlined-basic" label="Name" variant="filled" />
             <TextField id="outlined-basic" label="Email" variant="filled" />
             <Button variant="contained">Submit</Button>
