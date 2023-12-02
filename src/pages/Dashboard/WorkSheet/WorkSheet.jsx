0bdd521f7783c18a6c00a4f383d12064f6645ec0
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-02 10:26:17 +0600

    work-sheet form has been designed

--- src/pages/Dashboard/WorkSheet/WorkSheet.jsx
+++ src/pages/Dashboard/WorkSheet/WorkSheet.jsx
@@ -14,38 +14,53 @@ const WorkSheet = () => {
   return (
     <Box sx={{ py: "80px" }}>
       <Grid container justifyContent="center">
-          <Paper
-            component="form"
-            elevation={2}
-            sx={{
-              // minWidth: "450px",
-              padding: {
-                xs: 1,
-                md: 5
-              },
-              display: "flex",
-              gap: 1,
-              mx: "auto",
-            }}            
-          >
-            <FormControl fullWidth>
-              <InputLabel id="demo-simple-select-label">Tasks</InputLabel>
-              <Select
-                labelId="demo-simple-select-label"
-                id="demo-simple-select"
-                label="Tasks"
-                value=""
-              >
-                <MenuItem value={10}>A</MenuItem>
-                <MenuItem value={20}>B</MenuItem>
-                <MenuItem value={30}>C</MenuItem>
-              </Select>
-            </FormControl>
-            <TextField  InputLabelProps={{sx: {fontSize: {xs: 10, md: 16}}}} id="outlined-basic" label="Hours" variant="filled" />
-            <TextField  InputLabelProps={{sx: {fontSize: {xs: 10, md: 16}}}} id="outlined-basic" label="Date" variant="filled" />
-            <Button variant="contained">Add</Button>
-          </Paper>
-        </Grid>
+        <Paper
+          component="form"
+          elevation={2}
+          sx={{
+            minWidth: "450px",
+            marginLeft: {
+              xs: 16,
+            },
+            padding: {
+              xs: 1,
+              md: 5,
+            },
+            display: "flex",
+            gap: 1,
+            mx: "auto",
+          }}
+        >
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Tasks</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              label="Tasks"
+              value=""
+            >
+              <MenuItem value="sales">Sales</MenuItem>
+              <MenuItem value="support">Support</MenuItem>
+              <MenuItem value="content">Content</MenuItem>
+              <MenuItem value="paper-work">Paper Work</MenuItem>
+            </Select>
+          </FormControl>
+          <TextField
+            type="number"
+            id="outlined-basic"
+            label="Hours"
+            variant="filled"
+          />
+          <TextField
+            type="date"
+            id="outlined-basic"
+            label="Date"
+            variant="filled"
+            sx={{width: "100%"}}
+          />
+          <Button variant="contained">Add</Button>
+        </Paper>
+      </Grid>
     </Box>
   );
 };
