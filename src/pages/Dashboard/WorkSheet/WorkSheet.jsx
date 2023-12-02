1949e1caa6c64d9a7431097bb314e8d5cd735e0f
Nazmul1582 <nazmulhasan1582@gmail.com>
2023-12-02 11:02:09 +0600

    created a demo table of work-sheet and changed work-sheet form style

--- src/pages/Dashboard/WorkSheet/WorkSheet.jsx
+++ src/pages/Dashboard/WorkSheet/WorkSheet.jsx
@@ -2,67 +2,118 @@ import {
   Box,
   Button,
   FormControl,
-  Grid,
   InputLabel,
   MenuItem,
   Paper,
   Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
 } from "@mui/material";
 
-const WorkSheet = () => {
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+export default function WorkSheet() {
   return (
-    <Box sx={{ py: "80px" }}>
-      <Grid container justifyContent="center">
-        <Paper
-          component="form"
-          elevation={2}
-          sx={{
-            minWidth: "450px",
-            marginLeft: {
-              xs: 16,
-            },
-            padding: {
-              xs: 1,
-              md: 5,
-            },
-            display: "flex",
-            gap: 1,
-            mx: "auto",
-          }}
-        >
-          <FormControl fullWidth>
-            <InputLabel id="demo-simple-select-label">Tasks</InputLabel>
-            <Select
-              labelId="demo-simple-select-label"
-              id="demo-simple-select"
-              label="Tasks"
-              value=""
-            >
-              <MenuItem value="sales">Sales</MenuItem>
-              <MenuItem value="support">Support</MenuItem>
-              <MenuItem value="content">Content</MenuItem>
-              <MenuItem value="paper-work">Paper Work</MenuItem>
-            </Select>
-          </FormControl>
-          <TextField
-            type="number"
-            id="outlined-basic"
-            label="Hours"
-            variant="filled"
-          />
-          <TextField
-            type="date"
-            id="outlined-basic"
-            label="Date"
-            variant="filled"
-            sx={{width: "100%"}}
-          />
-          <Button variant="contained">Add</Button>
-        </Paper>
-      </Grid>
+    <Box>
+      <TableContainer sx={{mb: 5}}>
+        <Table sx={{ minWidth: 500 }} aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell></TableCell>
+            </TableRow>
+          </TableHead>
+
+          <TableBody>
+            <TableRow>
+              <TableCell>
+                <Paper
+                  component="form"
+                  elevation={2}
+                  sx={{
+                    padding: {
+                      xs: 2,
+                      md: 5,
+                    },
+                    display: "flex",
+                    gap: 1,
+                  }}
+                >
+                  <FormControl fullWidth>
+                    <InputLabel id="demo-simple-select-label">Tasks</InputLabel>
+                    <Select
+                      labelId="demo-simple-select-label"
+                      id="demo-simple-select"
+                      label="Tasks"
+                      value=""
+                    >
+                      <MenuItem value="sales">Sales</MenuItem>
+                      <MenuItem value="support">Support</MenuItem>
+                      <MenuItem value="content">Content</MenuItem>
+                      <MenuItem value="paper-work">Paper Work</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <TextField
+                    type="number"
+                    id="outlined-basic"
+                    label="Hours"
+                    variant="filled"
+                    fullWidth
+                  />
+                  <TextField
+                    type="date"
+                    id="outlined-basic"
+                    label="Date"
+                    variant="filled"
+                    fullWidth
+                  />
+                  <Button fullWidth variant="contained">Add</Button>
+                </Paper>
+              </TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+      <TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Tasks</TableCell>
+              <TableCell align="center">Hours</TableCell>
+              <TableCell align="center">Date</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+              >
+                <TableCell align="center" component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.calories}</TableCell>
+                <TableCell align="center">{row.fat}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
-};
-
-export default WorkSheet;
+}
